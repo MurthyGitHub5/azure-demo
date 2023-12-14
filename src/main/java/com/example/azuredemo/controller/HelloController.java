dc9f195fd4619f28f200676117de8e388e0856f7
MurthyGitHub5 <92693883+MurthyGitHub5@users.noreply.github.com>
2023-12-14 00:19:49 -0500

    Add files via upload

---
+++ src/main/java/com/example/azuredemo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.azuredemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Welcome to Azure Web App";
+    }
+}
